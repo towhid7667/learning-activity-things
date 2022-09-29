9ec00bab1d7035b05bcc42220b991f3cb9275a4e
towhid7667 <towhidulhq@gmail.com>
2022-09-29 20:37:38 +0600

    responsive done

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -5,18 +5,18 @@ import { faLocationArrow } from '@fortawesome/free-solid-svg-icons';
 const Info = () => {
     return (
         <div>
-            <div className='profile-area lg:flex lg:gap-3 lg:items-center md:flex md:gap-3 md:items-center my-10'>
+            <div className='profile-area lg:flex lg:gap-3 lg:items-center  md:items-center my-10 '>
                 <div className="avatar online">
                     <div className="w-24 rounded-full">
                          <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='' />
                     </div>
                  </div>
-                 <div>
+                 <div className=''>
                     <h1 className='text-2xl font-bold'>Towhidul</h1>
                     <h1 className='text-xl'><FontAwesomeIcon icon={faLocationArrow}/> Dhaka, Bangladesh</h1>
                  </div> 
             </div>
-            <div className="profile-info lg:flex md:flex text-center gap-5 bg-slate-200 px-5 py-3 rounded-lg">
+            <div className="profile-info lg:flex  text-center gap-5 bg-slate-200 px-5 py-3 rounded-lg">
                 <div>
                     <h1 className="lg:text-xl font-bold">8.5</h1>
                     <h1 className='text-1xl'>Speaking</h1>
@@ -39,27 +39,27 @@ const Info = () => {
 
             <div className="times-here">
                 <h1 className='lg:text-2xl md:text-2xl text-xl font-bold my-5'>Add Break Time</h1>
-                <div className="b-Times lg:flex md:flex bg-slate-200 rounded-lg gap-4 py-5 px-5 justify-center items-center text-center">
-                    <button className='rounded-[50%]  bg-white p-3 hover:bg-violet-400 hover:text-white font-bold my-3 lg:my-0 md:my-0'>10s</button>
-                    <button className='rounded-[50%]  bg-white p-3 hover:bg-violet-400 hover:text-white font-bold my-3 lg:my-0 md:my-0'>20s</button>
-                    <button className='rounded-[50%]  bg-white p-3 hover:bg-violet-400 hover:text-white font-bold my-3 lg:my-0 md:my-0'>30s</button>
-                    <button className='rounded-[50%]  bg-white p-3 hover:bg-violet-400 hover:text-white font-bold my-3 lg:my-0 md:my-0'>40s</button>
-                    <button className='rounded-[50%]  bg-white p-3 hover:bg-violet-400 hover:text-white font-bold my-3 lg:my-0 md:my-0'>50s</button>
+                <div className="b-Times lg:flex  bg-slate-200 rounded-lg gap-4 py-5 px-5 justify-center items-center text-center">
+                    <button className='rounded-[50%]  bg-white p-3 hover:bg-violet-400 hover:text-white font-bold my-3 lg:my-0 md:my-3'>10s</button>
+                    <button className='rounded-[50%]  bg-white p-3 hover:bg-violet-400 hover:text-white font-bold my-3 lg:my-0 md:my-3'>20s</button>
+                    <button className='rounded-[50%]  bg-white p-3 hover:bg-violet-400 hover:text-white font-bold my-3 lg:my-0 md:my-3'>30s</button>
+                    <button className='rounded-[50%]  bg-white p-3 hover:bg-violet-400 hover:text-white font-bold my-3 lg:my-0 md:my-3'>40s</button>
+                    <button className='rounded-[50%]  bg-white p-3 hover:bg-violet-400 hover:text-white font-bold my-3 lg:my-0 md:my-3'>50s</button>
                 </div>
             </div>
             <div>
                 <h1 className='text-2xl font-bold my-5'>Activity Details</h1>
-                <div className='lg:flex md:flex bg-slate-200 px-4 py-3 rounded-lg justify-around my-5 text-center'>
+                <div className='lg:flex  bg-slate-200 px-4 py-3 rounded-lg justify-around my-5 text-center items-center'>
                     <h1 className='text-xl font-bold'>Activity Time</h1>
                     <p className='font-bold my-3'>200s</p>
                 </div>
-                <div className='lg:flex md:flex bg-slate-200 px-4 py-3 rounded-lg justify-around my-5 text-center'>
+                <div className='lg:flex  bg-slate-200 px-4 py-3 rounded-lg justify-around my-5 text-center items-center'>
                     <h1 className='text-xl font-bold'>Break Time</h1>
                     <p className='font-bold my-3'>200s</p>
                 </div>
             </div>
             <div className='flex justify-center items-center'>
-            <button className='bg-purple-700 text-xl lg:text-2xl md:text-2xl text-white font-bold lg:px-10 px-4 py-3 rounded-lg'>Completed</button>
+            <button className='bg-purple-700 text-xl lg:text-2xl md:text-2xl text-white font-bold lg:px-10 px-4 py-3 rounded-lg hover:bg-purple-900'>Completed</button>
             </div>
             
         </div>
--- src/components/activity-here/Actiivities.js
+++ src/components/activity-here/Actiivities.js
@@ -11,20 +11,20 @@ const Actiivities = () => {
         fetch("fakeData.json")
         .then(res => res.json())
         .then(data => setActivities(data))
-    }, [])
+    }, []);
     return (
         <div className='grid grid-cols-[4fr,1fr] bg-slate-200'>
             <div className='activities-here my-32 mx-auto'>
-                <h1 className='text-2xl lg:text-4xl md:text-3xl text-orange-600 font-bold  lg:text-center md:text-center mx-10 lg:mx-0 md:mx-0'><FontAwesomeIcon icon={faChartLine}/> Activiy Things</h1>
-                <h1 className='text-1xl lg:text-2xl md:text-xl text-violet-600 font-bold lg:text-center md:text-center mx-14 lg:mx-0 md:mx-0'>Select Todays Plan</h1>
-                <div className='grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 gap-5 lg:mx-auto md:mx-auto mx-5 w-8/12 lg:w-10/12 md:w-9/12 my-5'>
+                <h1 className='text-2xl lg:text-4xl md:text-3xl text-orange-600 font-bold  lg:text-center  mx-10 md:mx-32  lg:mx-0'><FontAwesomeIcon icon={faChartLine}/> Activiy Things</h1>
+                <h1 className='text-1xl lg:text-2xl md:text-xl text-violet-600 font-bold lg:text-center  mx-14 lg:mx-0 md:mx-40'>Select Todays Plan</h1>
+                <div className='grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 gap-5 lg:mx-auto md:mx-6 mx-5 w-8/12 lg:w-10/12 md:w-9/12 my-5'>
                 {
                     activities.map(activity => <Activity activity = {activity} /> )
                 }
                 </div>
 
             </div>
-            <div className='info-here w-3/12 bg-white px-20 py-8 absolute right-0 flex flex-col items-center justify-center'>
+            <div className='info-here w-3/12 md:w-4/12 bg-white px-20 py-8 absolute right-0 flex flex-col items-center justify-center'>
                 <Info />
             </div>
             
