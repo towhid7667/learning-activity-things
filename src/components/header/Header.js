946318c681df9280d1dea8cf171b0747f820adb9
towhid7667 <towhidulhq@gmail.com>
2022-09-29 13:58:05 +0600

    Project started

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='text-orange-400 text-2xl'>
+            <h1>Ki khbr???</h1>
+        </div>
+    );
+};
+
+export default Header;
